9209eca437fd4d47b418094df58ba99d91db72b5
imranslab12 <imrananwar3124@gmail.com>
2025-03-12 23:53:37 +0000

    Initial commit

---
+++ test.pl
@@ -0,0 +1,42 @@
+% child_of(Child, Parent)
+child_of(bart, homer).
+child_of(lisa, homer).
+child_of(maggie, homer).
+child_of(homer, abe).
+child_of(herb, abe).
+child_of(abby, mona).
+child_of(homer, mona).
+child_of(herb, mona).
+child_of(ling, selma).
+child_of(selma, jackie).
+child_of(patty, jackie).
+
+% male(Person)
+male(abe).
+male(homer).
+male(bart).
+
+% female(Person)
+female(mona).
+female(herb).
+female(maggie).
+female(lisa).
+female(abby).
+female(selma).
+female(patty).
+female(ling).
+female(jackie).
+
+% age_is(Person, Age)
+age_is(abe, 80).
+age_is(mona, 75).
+age_is(homer, 40).
+age_is(herb, 45).
+age_is(bart, 10).
+age_is(lisa, 8).
+age_is(maggie, 1).
+age_is(abby, 42).
+age_is(selma, 44).
+age_is(patty, 44).
+age_is(ling, 12).
+age_is(jackie, 70).
